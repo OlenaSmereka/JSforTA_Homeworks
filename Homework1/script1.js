7f8058244c1980cf3ce5b2b214820a435d5b254b
Olena Smereka <osmer@softserveinc.com>
2024-05-27 01:34:30 +0300

    first commit

---
+++ Homework1/script1.js
@@ -0,0 +1,76 @@
+// Second task 
+
+// console.log('Smereka');
+
+// Third task
+
+// let name='Olena';
+// let age=37;
+// console.log(name);
+// console.log(age);
+// name=age;
+// console.log(name);
+// console.log(age);
+
+// Fourth task
+
+// const user={
+//     Name:'Olena',
+//     Age:37,
+//     IsFemale:true,
+//     IsMarried:undefined,
+//     Children:null
+// }
+// console.log(user);
+
+// Fifth task
+
+// let isAdult=confirm('Are you above 18?');
+// console.log(isAdult);
+
+// Sixth task
+
+// let FirstName='Olena', LastName='Smereka',StudyGroup=301, YearOFbirth=1986, isMarried=true, Children=null,Nationality=undefined;
+// console.log(typeof StudyGroup, StudyGroup);
+// console.log(typeof YearOFbirth,YearOFbirth);
+// console.log(typeof isMarried,isMarried);
+// console.log(typeof FirstName, FirstName);
+// console.log(typeof LastName, LastName);
+// console.log(typeof Children, Children);
+// console.log(typeof Nationality, Nationality);
+
+// OR
+// let FirstName, LastName,StudyGroup, YearOFbirth, isMarried,Children,Nationality;
+//     FirstName='Olena';
+//     LastName='Smereka';
+//     StudyGroup=301; 
+//     YearOFbirth=1986; 
+//     isMarried=true;
+//     Children=null;
+//     Nationality=undefined;
+// console.log(typeof StudyGroup, StudyGroup);
+// console.log(typeof YearOFbirth,YearOFbirth);
+// console.log(typeof isMarried,isMarried);
+// console.log(typeof FirstName, FirstName);
+// console.log(typeof LastName, LastName);
+// console.log(typeof Children, Children);
+// console.log(typeof Nationality, Nationality);
+
+// Seventh task
+
+// let login=prompt('Enter your login?');
+// let email=prompt('Enter your email?');
+// let password=prompt('Enter your password?');
+// console.log("'Dear "+ login + ", your email is " + email + "," + ` your password is ${password}.'`);
+
+// Eight task
+
+// const MIN=60
+// let Hour=MIN*60, Day=Hour*24, Month=Day*31;
+// console.log('Seconds in an hour ' + Hour);
+// console.log('Seconds in a day ' + Day);
+// console.log('Seconds in a month ' + Month);
+
+
+
+
---
+++ topic1.js
@@ -0,0 +1,41 @@
+//  alert('Hello from JS file!');
+
+//  console.log('Hello from console');
+
+// let name="Olena";
+// let age=37;
+// // name="Sofia"
+// name=age; 
+// console.log(name);
+// console.log(age);  
+
+// const NUMBER=2000;
+// NUMBER=3000
+// console.log(NUMBER); 
+
+// let if=5;
+
+// const user={
+//     name:'Olena',
+//     age:37,
+//     isMarried:true,
+//     some: undefined
+// }
+// console.log(user); 
+ 
+// prompt('How old are you?');
+
+// let name=prompt('What is your name?');
+// console.log (name);
+
+// let name=prompt('What is your name?','name here');
+// console.log (name);
+
+// let ok=confirm('Are you ok?');
+// console.log(ok);
+
+let name=prompt('What is your name?');
+// console.log('Your name is '+ name); 
+// console.log('Ole'+'na');
+
+// console.log(`Your name is ${name}`);
