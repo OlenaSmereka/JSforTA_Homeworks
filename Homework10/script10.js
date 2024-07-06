1610749cdcd6384c5d215d0adf2614299baa7319
Olena Smereka <smerekaalena@gmail.com>
2024-07-06 17:19:19 +0300

    update code

--- Homework10/script10.js
+++ Homework10/script10.js
@@ -1,29 +1,29 @@
-// Task1
-const http=require("http");
-const os=require("os");
-const path=require("path");
+// // Task1
+// const http=require("http");
+// const os=require("os");
+// const path=require("path");
 
 
-http.createServer((request,response)=> {
-    response.writeHead(200,"Content-Type: text/html");
-    const user=os.userInfo().username;
-    const OStype=os.type();
-    const timeMinutes=Math.floor(os.uptime()/60);
-    const currWD=process.cwd();
-    const SFname=path.basename(__filename);
-    response.write(`
-        <h1>Information</h1>
-        <ul>
-            <li>Current user: ${user}</li>
-            <li>Type of the operating system: ${OStype}</li>
-            <li>Time in minutes: ${timeMinutes}</li>
-            <li>Current working directory: ${currWD}</li>
-            <li>Name of the server file: ${SFname}</li>
-        </ul>
-        `);
-    response.end();
-}).listen(5000);
-console.log("The server is listening on http://localhost:5000");
+// http.createServer((request,response)=> {
+//     response.writeHead(200,"Content-Type: text/html");
+//     const user=os.userInfo().username;
+//     const OStype=os.type();
+//     const timeMinutes=Math.floor(os.uptime()/60);
+//     const currWD=process.cwd();
+//     const SFname=path.basename(__filename);
+//     response.write(`
+//         <h1>Information</h1>
+//         <ul>
+//             <li>Current user: ${user}</li>
+//             <li>Type of the operating system: ${OStype}</li>
+//             <li>Time in minutes: ${timeMinutes}</li>
+//             <li>Current working directory: ${currWD}</li>
+//             <li>Name of the server file: ${SFname}</li>
+//         </ul>
+//         `);
+//     response.end();
+// }).listen(5000);
+// console.log("The server is listening on http://localhost:5000");
 
 
 // Task2
@@ -36,7 +36,7 @@ http.createServer((request,response)=> {
     const greetingMessage = UserGreetings();
 response.write(`
     <p>Request date: ${requestDate}</p>
-    <p>Request date: ${greetingMessage}</p>
+    <p>${greetingMessage}</p>
     `);
 response.end();
 }).listen(8000);
